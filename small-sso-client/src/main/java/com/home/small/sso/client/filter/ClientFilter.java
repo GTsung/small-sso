67dd939cf5de83f7185e80ccfded2b008e22defd
GTsung <xc_goo@163.com>
2021-10-30 22:40:23 +0800

    抽象过滤器基类

--- small-sso-client/src/main/java/com/home/small/sso/client/filter/ClientFilter.java
+++ small-sso-client/src/main/java/com/home/small/sso/client/filter/ClientFilter.java
@@ -1,14 +1,34 @@
 package com.home.small.sso.client.filter;
 
+import com.home.small.sso.client.listener.LogoutListener;
+import com.home.small.sso.client.session.SessionMappingStorage;
+
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * filter基类
+ *
  * @author GTsung
  * @date 2021/10/30
  */
-public class ClientFilter extends ParamFilter implements Filter {
+public abstract class ClientFilter extends ParamFilter implements Filter {
+
+    private SessionMappingStorage sessionMappingStorage;
+
+    /**
+     * 是否已经授权通过
+     *
+     * @param request
+     * @param response
+     * @return
+     * @throws IOException
+     */
+    public abstract boolean isAccessAllowed(HttpServletRequest request, HttpServletResponse response)
+            throws IOException;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -23,5 +43,12 @@ public class ClientFilter extends ParamFilter implements Filter {
     @Override
     public void destroy() {
 
+    }
+
+    protected SessionMappingStorage getSessionMappingStorage() {
+        if (Objects.isNull(sessionMappingStorage)) {
+            sessionMappingStorage = LogoutListener.getSessionMappingStorage();
+        }
+        return sessionMappingStorage;
     }
 }
